ff7be76d03d6e5e3fb418cb16d4892f5d7afe4b7
Gabor Angyal <angyalgabor@outlook.com>
2021-07-30 11:30:42 +0200

    regenerate project with Vue 3

--- files/src/main.js
+++ files/src/main.js
@@ -1,30 +1,4 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify'
 
-Vue.config.productionTip = false
-
-store.dispatch('init');
-
-Vue.filter('dateFormat', function(date){
-  return `${date.toLocaleDateString('en-US')}, ${date.toLocaleTimeString('en-US')}`;
-})
-
-new Vue({
-  router,
-  store,
-  vuetify,
-  mounted: async function() {
-    if(store.state.auth.jwt){
-      this.$store.state.auth.user = await this.$store.dispatch('profile');
-    }
-  },
-  data: function () {
-    return {
-      user: null
-    };
-  },
-  render: h => h(App)
-}).$mount('#app')
+createApp(App).mount('#app')
