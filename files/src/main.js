72e5cb463dab3c415b71549f0efbf43f9f79b8c5
Gábor Angyal <gabor.angyal@codesharp.hu>
2021-08-03 18:10:34 +0200

    fix app init

--- files/src/main.js
+++ files/src/main.js
@@ -2,12 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-const app = createApp(App).use(store).use(router)
 
-app.config.globalProperties.$filters = {
-    dateFormat(date) {
-        return `${date.toLocaleDateString('en-US')}, ${date.toLocaleTimeString('en-US')}`;
+store.dispatch('init').then(() => {
+    const app = createApp(App).use(store).use(router)
+    app.config.globalProperties.$filters = {
+        dateFormat(date) {
+            return `${date.toLocaleDateString('en-US')}, ${date.toLocaleTimeString('en-US')}`;
+        }
     }
-}
-
-store.dispatch('init').then(() => app.mount('#app'))
+    app.mount('#app')
+})
