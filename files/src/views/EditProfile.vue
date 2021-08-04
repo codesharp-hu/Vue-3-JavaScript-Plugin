4e7780566cd96edab28a0047325fe72526274bcc
orsos <orsoslivia@hotmail.com>
2021-08-04 13:34:01 +0200

    fix edit profile

--- files/src/views/EditProfile.vue
+++ files/src/views/EditProfile.vue
@@ -2,13 +2,14 @@
   <div class="container-fluid">
     <form>
       <div class="mb-3">
-        <label for="name" class="form-label">Name</label>
+        <label for="userName" class="form-label">Name</label>
         <input
           v-model="userName"
           type="text"
           class="form-control"
-          id="name"
+          id="userName"
         />
+        <span>{{ userNameError }}</span>
       </div>
       <div class="mb-3">
         <label for="email" class="form-label">Email</label>
@@ -18,12 +19,10 @@
           class="form-control"
           id="email"
         />
+        <span>{{ emailError }}</span>
       </div>
       <div class="alert alert-danger" role="alert" v-if="errorMsg">
         {{ errorMsg }}
-      </div>
-      <div class="alert alert-success" role="alert" v-if="successMsg">
-        {{ successMsg }}
       </div>
       <input
         class="btn btn-primary"
@@ -39,11 +38,12 @@
 import { defineComponent, ref } from 'vue';
 import { useForm, useField } from 'vee-validate';
 import { useStore } from 'vuex';
+import { useRouter } from 'vue-router';
 import api from '../api';
 export default defineComponent({
   setup() {
     const simpleSchema = {
-      name(value) {
+      userName(value) {
         if (value && value.trim()) {
           return true;
         }
@@ -59,17 +59,21 @@ export default defineComponent({
     const { handleSubmit } = useForm({
       validationSchema: simpleSchema,
     });
-    const { value: userName, errorMessage: userNameError } = useField('name');
+    const { value: userName, errorMessage: userNameError } = useField('userName');
     const { value: email, errorMessage: emailError } = useField('email');
     let store = useStore();
+    userName.value = store.state.auth.user.userName;
+    email.value = store.state.auth.user.email;
     let errorMsg = ref('');
-    let successMsg = ref('');
+    const router = useRouter();
     const onSubmit = handleSubmit(async () => {
       try {
         await api.updateProfile({
           userName: userName.value,
           email: email.value
         }, store.state.auth.jwt);
+        store.dispatch('profile');
+        router.push('profile');
       } catch (reason) {
         if (reason.isUserNameInUse) {
           errorMsg.value = 'This user name already exists!';
@@ -77,9 +81,6 @@ export default defineComponent({
           errorMsg.value = 'This email already exists!';
         } else {
           errorMsg.value = '';
-        }
-        if (reason.success) {
-          successMsg.value = 'Your profile is successfuly updated.'
         }
       }
     });
@@ -89,8 +90,7 @@ export default defineComponent({
       userNameError,
       email,
       emailError,
-      errorMsg,
-      successMsg
+      errorMsg
     };
   },
 });
