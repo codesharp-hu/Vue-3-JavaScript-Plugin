ea030b2a7cb13d65ab21bb9cf2733e2b09782877
Lívia Orsós <livia.orsos@codesharp.hu>
2021-08-25 09:32:08 +0200

    update main.js

--- files/src/main.js
+++ files/src/main.js
@@ -1,14 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
 
-store.dispatch('init').then(() => {
-    const app = createApp(App).use(store).use(router)
-    app.config.globalProperties.$filters = {
-        dateFormat(date) {
-            return `${date.toLocaleDateString('en-US')}, ${date.toLocaleTimeString('en-US')}`;
-        }
+const app = createApp(App)
+app.config.globalProperties.$filters = {
+    dateFormat(date) {
+        return `${date.toLocaleDateString('en-US')}, ${date.toLocaleTimeString('en-US')}`;
     }
-    app.mount('#app')
-})
+}
+app.mount('#app')
